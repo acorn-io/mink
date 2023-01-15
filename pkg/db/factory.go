60a33c077d5c6a8399e8706cdbbc8b3c76ee2388
Darren Shepherd <darren@acorn.io>
2023-01-14 20:11:13 -0700

    Add SQL trace logging

--- pkg/db/factory.go
+++ pkg/db/factory.go
@@ -1,12 +1,17 @@
 package db
 
 import (
+	"log"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/acorn-io/mink/pkg/strategy"
 	"github.com/acorn-io/mink/pkg/types"
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
@@ -46,8 +51,18 @@ func (f *Factory) NewDBStrategy(obj types.Object) (strategy.CompleteStrategy, er
 		tableName string
 	)
 	if f.db != nil {
+		level := logger.Warn
+		if logrus.IsLevelEnabled(logrus.TraceLevel) {
+			level = logger.Info
+		}
 		gdb, err = gorm.Open(f.db, &gorm.Config{
 			SkipDefaultTransaction: true,
+			Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+				SlowThreshold:             200 * time.Millisecond,
+				LogLevel:                  level,
+				IgnoreRecordNotFoundError: false,
+				Colorful:                  true,
+			}),
 		})
 		if err != nil {
 			return nil, err
