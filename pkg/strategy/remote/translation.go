53e7a2dd56cef083fa3a810d391b3fa38909e47c
Donnie Adams <donnie@acorn.io>
2023-03-22 15:37:49 -0400

    Adjust objects to have correct public GVK
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/strategy/remote/translation.go
+++ pkg/strategy/remote/translation.go
@@ -3,6 +3,7 @@ package remote
 import (
 	"github.com/acorn-io/mink/pkg/strategy"
 	"github.com/acorn-io/mink/pkg/strategy/translation"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
@@ -15,14 +16,11 @@ func NewWithSimpleTranslation(translator translation.SimpleTranslator, pub kclie
 
 	remote := NewRemote(priv, privList, kclient)
 	simpleTranslator := translation.NewSimpleTranslator(translator, pub, pubList)
-	return translation.NewTranslationStrategy(simpleTranslator, remote)
+	return translation.NewTranslationStrategy(simpleTranslator, remote, getGVK(pub, kclient))
 }
 
 func getList(obj kclient.Object, watch kclient.WithWatch) kclient.ObjectList {
-	gvk, err := apiutil.GVKForObject(obj, watch.Scheme())
-	if err != nil {
-		panic(err)
-	}
+	gvk := getGVK(obj, watch)
 	gvk.Kind += "List"
 	objList, err := watch.Scheme().New(gvk)
 	if err != nil {
@@ -33,5 +31,13 @@ func getList(obj kclient.Object, watch kclient.WithWatch) kclient.ObjectList {
 
 func NewWithTranslation(translator translation.Translator, obj kclient.Object, kclient kclient.WithWatch) strategy.CompleteStrategy {
 	remote := NewRemote(obj, getList(obj, kclient), kclient)
-	return translation.NewTranslationStrategy(translator, remote)
+	return translation.NewTranslationStrategy(translator, remote, getGVK(obj, kclient))
+}
+
+func getGVK(obj kclient.Object, watch kclient.WithWatch) schema.GroupVersionKind {
+	gvk, err := apiutil.GVKForObject(obj, watch.Scheme())
+	if err != nil {
+		panic(err)
+	}
+	return gvk
 }
